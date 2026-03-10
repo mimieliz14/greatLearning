d4cc26ee8e0e6a7777ef1feaa2483c6ef44c525f
NOEMI ELIZABETH OROZCO PILCO <noemi.orozco@iess.gob.ec>
2026-03-10 16:56:26 -0500

    conitinue

--- Week3_TypeScript/my-next-app/app/pages/login.tsx
+++ Week3_TypeScript/my-next-app/app/pages/login.tsx
@@ -1,5 +1,11 @@
+"use cliente"
+
+import { useState } from "react"
+import { loginUser } from "@/lib/auth"
 
 export default function Login(){
+    const [username,setUsername] = useState("")
+    const [password, setPassword] = useState("")
     return(<>
     <form>
         <input type="text" placeholder=""></input>
